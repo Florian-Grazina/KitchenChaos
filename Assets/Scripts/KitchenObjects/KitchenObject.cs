7e400888bc9aa104121dc7d44659a244e647624d
Florian Grazina <Floriangrazina@gmail.com>
2025-05-31 13:10:50 +0200

    add so public get

--- Assets/Scripts/KitchenObjects/KitchenObject.cs
+++ Assets/Scripts/KitchenObjects/KitchenObject.cs
@@ -2,15 +2,7 @@ using UnityEngine;
 
 public class KitchenObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public KitchenObjectSO GetKitchenObjectSO() => kitchenObjectSO;
 }
