980b96d386a69ef702ff84698b12a7e6fb9619f3
Florian Grazina <Floriangrazina@gmail.com>
2025-05-31 12:49:25 +0200

    add tomato prefab

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform counterTopPoint;
+
+
     public void Interact()
     {
         Debug.Log("Clearing counter...");
+        Transform tomato = Instantiate(tomatoPrefab, counterTopPoint);
+        tomato.localPosition = Vector3.zero;
     }
 }
