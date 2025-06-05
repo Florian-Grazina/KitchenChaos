2e2a1228c93a3461a57b8a28a5a601a1f3cbd157
Florian Grazina <Floriangrazina@gmail.com>
2025-06-05 20:07:55 +0200

    create Kitchen RecipieSO

---
+++ Assets/Scripts/KitchenObjects/CuttingRecipeSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu()]    
+public class CuttingRecipeSO : ScriptableObject
+{
+    public KitchenObjectSO inputKitchenObjectSO;
+    public KitchenObjectSO outputKitchenObjectSO;
+}
