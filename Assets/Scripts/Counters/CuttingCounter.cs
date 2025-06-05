81948ce6cd7e990ab24c5559fbcb3fcb1474ca3b
Florian Grazina <Floriangrazina@gmail.com>
2025-06-05 20:20:56 +0200

    add check to avoid putting items that can't be cut

--- Assets/Scripts/Counters/CuttingCounter.cs
+++ Assets/Scripts/Counters/CuttingCounter.cs
@@ -12,7 +12,13 @@ public class CuttingCounter : BaseCounter
         {
             // player has an object, drop it
             if (player.HasKitchenObject())
-                player.GetKitchenObject().SetKitchenObjectHolder(this);
+            {
+                // if the kitchen object on the player can be cut
+                if (HasCuttingRecipe(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    player.GetKitchenObject().SetKitchenObjectHolder(this);
+                }
+            }
         }
         // counter has an object
         else
@@ -40,6 +46,11 @@ public class CuttingCounter : BaseCounter
                 }
             }
         }
+    }
+
+    private bool HasCuttingRecipe(KitchenObjectSO inputKitchenObjectSO)
+    {
+        return cuttingRecipeSOArray.Any(cuttingRecipe => cuttingRecipe.inputKitchenObjectSO == inputKitchenObjectSO);
     }
 
     private KitchenObjectSO GetCuttingRecipeOutput(KitchenObjectSO inputKitchenObjectSO)
