2a2e17af72771bf394fd9f190aa228c52685f7f8
Florian Grazina <Floriangrazina@gmail.com>
2025-06-09 23:45:36 +0200

    add look at camera inverted

--- Assets/Scripts/Counters/LookAtCamera.cs
+++ Assets/Scripts/Counters/LookAtCamera.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class LookAtCalera : MonoBehaviour
 {
+    private enum Mode
+    {
+        LookAt,
+        LookAtInverted
+    }
+
+    [SerializeField] private Mode mode;
+
     protected void LateUpdate()
     {
-        transform.LookAt(Camera.main.transform.position);
+        switch (mode)
+        {
+            case Mode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case Mode.LookAtInverted:
+                Vector3 dirFromCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + dirFromCamera);
+                break;
+
+        }
     }
 }
