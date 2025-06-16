e187a366671031509e161bee1fbc67b11b9302ae
Florian Grazina <Floriangrazina@gmail.com>
2025-06-16 21:14:11 +0200

    add trash script

---
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TrashCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+
+    public override void InteractAlternate(Player player)
+    {
+    }
+}
