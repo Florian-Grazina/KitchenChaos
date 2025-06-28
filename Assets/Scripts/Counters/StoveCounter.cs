b727abb3030cf89421b37fbbca355cccc5d75295
Florian Grazina <Floriangrazina@gmail.com>
2025-06-28 14:42:57 +0200

    stove script with frying recipe

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -1,16 +1,63 @@
+using Assets.Scripts.Events;
+using System.Linq;
 using UnityEngine;
 
 public class StoveCounter : BaseCounter
 {
+    [SerializeField] private FryingRecipeSO[] fryingRecipeSOArray;
 
-
+    #region public methods
     public override void Interact(Player player)
     {
-        throw new System.NotImplementedException();
+        // counter has no object
+        if (!HasKitchenObject())
+        {
+            // player has an object
+            if (player.HasKitchenObject())
+            {
+                // if the kitchen object on the player can be fried
+                if (HasFryingRecipe(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    // drop it
+                    player.GetKitchenObject().SetKitchenObjectHolder(this);
+                }
+            }
+        }
+        // counter has an object
+        else
+        {
+            //player has no object, pick it up
+            if (!player.HasKitchenObject())
+                GetKitchenObject().SetKitchenObjectHolder(player);
+        }
     }
 
     public override void InteractAlternate(Player player)
     {
         throw new System.NotImplementedException();
     }
+    #endregion
+
+    #region private methods
+    private KitchenObjectSO GetFryingRecipeOutput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipe = GetFryingRecipe(inputKitchenObjectSO);
+
+        if (fryingRecipe != null)
+            return fryingRecipe.outputKitchenObjectSO;
+
+        Debug.LogWarning($"No cutting recipe found for {inputKitchenObjectSO}");
+        return null;
+    }
+
+    private bool HasFryingRecipe(KitchenObjectSO inputKitchenObjectSO)
+    {
+        return fryingRecipeSOArray.Any(fryingRecipe => fryingRecipe.inputKitchenObjectSO == inputKitchenObjectSO);
+    }
+
+    private FryingRecipeSO GetFryingRecipe(KitchenObjectSO inputKitchenObjectSO)
+    {
+        return fryingRecipeSOArray.FirstOrDefault(cuttingRecipe => cuttingRecipe.inputKitchenObjectSO == inputKitchenObjectSO);
+    }
+    #endregion
 }
