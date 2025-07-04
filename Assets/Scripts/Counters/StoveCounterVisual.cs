aaf5a6c62da04138cf57a7f17190ac12d340714a
Florian Grazina <Floriangrazina@gmail.com>
2025-07-05 00:26:46 +0200

    add frying animation to stove counter

--- Assets/Scripts/Counters/StoveCounterVisual.cs
+++ Assets/Scripts/Counters/StoveCounterVisual.cs
@@ -1,7 +1,22 @@
+using System;
 using UnityEngine;
+using static StoveCounter;
 
 public class StoveCounterVisual : MonoBehaviour
 {
+    [SerializeField] private StoveCounter stoveCounter;
     [SerializeField] private GameObject stoveOnGameObject;
     [SerializeField] private GameObject stoveParticleGameObject;
+
+    protected void Start()
+    {
+        stoveCounter.OnStateChanged += StoveCounter_OnStateChanged;
+    }
+
+    private void StoveCounter_OnStateChanged(object sender, OnStatechangedEventArgs e)
+    {
+        bool showVisual = e.state == StateEnum.Frying || e.state == StateEnum.Fried;
+        stoveOnGameObject.SetActive(showVisual);
+        stoveParticleGameObject.SetActive(showVisual);
+    }
 }
