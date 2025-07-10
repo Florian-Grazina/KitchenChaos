408c1d6f6f0c5ed07789a4f63b77d140ad591f28
Florian Grazina <Floriangrazina@gmail.com>
2025-07-10 20:54:16 +0200

    bug fix plate

--- Assets/Scripts/KitchenObjects/PlateKitchenObject.cs
+++ Assets/Scripts/KitchenObjects/PlateKitchenObject.cs
@@ -1,24 +1,40 @@
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    #region field
     [SerializeField]
-    private List<KitchenObjectSO> valideKitchenObjectSOList;
+    private List<KitchenObjectSO> validKitchenObjectSOList;
     private List<KitchenObjectSO> kitchenObjectSOList;
+    #endregion
 
+    #region unity methods   
     protected void Awake()
     {
         kitchenObjectSOList = new List<KitchenObjectSO>();
     }
+    #endregion
 
+    #region public methods
     public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
     {
-        if(kitchenObjectSOList.Contains(kitchenObjectSO))
+        if (!validKitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            Debug.LogError($"KitchenObjectSO {kitchenObjectSO.objectName} is not valid for this plate!");
             return false;
+        }
+
+        if (kitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            Debug.LogError($"KitchenObjectSO {kitchenObjectSO.objectName} is already on this plate!");
+            return false;
+        }
 
         kitchenObjectSOList.Add(kitchenObjectSO);
         return true;
     }
+    #endregion
 }
