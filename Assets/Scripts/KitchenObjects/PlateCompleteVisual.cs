a83955f09e2b9dae233d494e4bdecdbc47f618f3
Florian Grazina <Floriangrazina@gmail.com>
2025-07-27 23:48:47 +0200

    add struct for Kitchen object SO

--- Assets/Scripts/KitchenObjects/PlateCompleteVisual.cs
+++ Assets/Scripts/KitchenObjects/PlateCompleteVisual.cs
@@ -1,8 +1,26 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateCompleteVisual : MonoBehaviour
 {
+    [Serializable]
+    public struct KitchenObjectSO_GameObject
+    {
+        public KitchenObjectSO kitchenObjectSO;
+        public GameObject gameObject;
+    }
+
     [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private List<KitchenObjectSO_GameObject> kitchenObjectSOGameObjectList;
 
+    protected void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+    }
 
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        
+    }
 }
